40aa0ccf3df3ca105a07b0586ceee9d30062cb48
Christopher Morrison <Chris.Morrison500@gmail.com>
2017-11-20 13:04:12 -0500

    Added outline for data analysis script

---
+++ data_filter.py
@@ -0,0 +1,47 @@
+#############################################################
+### This file is for parsing the output of the html dump  ###
+#############################################################
+
+
+
+# read in all the data from filename and run the first
+# half of count sort on it to create a dictionary
+def readDump(filename):
+    return
+    # open file
+    # new dictionary
+    # for every word in file
+        # sequencial capital words are one key
+        
+        # single lowercase words are one key
+        
+        # if the key isn't in the list, add it
+
+        # increase key value by one
+    # return the dictionary
+
+
+# remove words from a list if filter(word) is true
+def removeByFilter(wordlist, filter)
+    # make new wordlist
+    #for word in wordlist
+        # if not filter(word)
+            # newlist.append word
+    #return new list
+    return
+
+# extract words from a list if filter(word) is true
+def extractByFilter(wordlist, filter)
+    # make new wordlist
+    #for word in wordlist
+        # if filter(word)
+            # newlist.append word
+    #return new list
+    return
+
+
+#return true if word is one of the 100 most common words in the english language
+def is_common_word(word):
+    #if word in commons
+    return
+
