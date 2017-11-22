056a71188e89467580d0b85280c706b31224221a
Will Osbourne <willosbourne@sevenhillstechnology.com>
2017-11-21 19:37:08 -0500

    web-scrape removing unused code

--- web-scrape.py
+++ web-scrape.py
@@ -2,33 +2,22 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 # Utility Functions 
-
-def find_between( s, first, last ):
-    try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
-        return s[start:end]
-    except ValueError:
-        return ""
-
 def print_file(printStr):
     with open("article.txt", "w") as text_file:
         print(printStr, file=text_file)
 
-
 # End Utility Functions
 
 urlstring = 'https://www.wired.com/story/self-care-digital-detox/'
-printVal = 'Empty Val'
 
+# Get Page Source Data
 response = urllib.request.urlopen(urlstring)
 page_source = response.read()
 
+# Parse HTML, isolate article, convert to string
 page_source_soup = BeautifulSoup(page_source, 'html.parser')
 article_text = page_source_soup.article
-
 article_text = article_text.get_text()
 
-
-
+# Print File for data analysis
 print_file(article_text)
