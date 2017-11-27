bea5c478a856efac72536765f97fc226c152cabe
Christopher Morrison <Chris.Morrison500@gmail.com>
2017-11-27 17:19:27 -0500

    I did it

--- data_filter.py
+++ data_filter.py
@@ -1,21 +1,17 @@
 #############################################################
 ### This file is for parsing the output of the html dump  ###
 #############################################################
-
-
+commons = open("./20k_common_english_words.txt")
+commons = commons.read().split()
 
 # read in all the data from filename and run the first
 # half of count sort on it to create a dictionary
 def readDump(filename):
-    return
     # open file
-    file = open(filename, "r")
-    print(file)
-    print("test")
+    file = open(filename)
     
     # new dictionary
     wordcount = {}
-    wordcount
 
     # tracking important phrases requires this
     phrase = ""
@@ -35,37 +31,55 @@ def readDump(filename):
             # if phrase is not ""
             if not phrase == "":
                 # add the phrase to the dictionary and reset it
+                if not phrase in wordcount:
+                    wordcount[phrase] = 0
                 wordcount[phrase] += 1
                 phrase = ""
 
             # add the word to the phrase
+            if not word in wordcount:
+                wordcount[word] = 0
             wordcount[word] += 1
 
     # return the dictionary
     return wordcount
 
-
 # remove words from a list if filter(word) is true
-def removeByFilter(wordlist, filter):
+def removeByFilter(wordlist, filter_function):
     # make new wordlist
+    newwordlist = {}
+
     #for word in wordlist
+    for word in wordlist:
         # if not filter(word)
+        if not filter_function(word):
             # newlist.append word
+            newwordlist[word] = wordlist[word]
+    
     #return new list
-    return
+    return newwordlist
 
 # extract words from a list if filter(word) is true
-def extractByFilter(wordlist, filter):
+def extractByFilter(wordlist, filter_function):
     # make new wordlist
-    #for word in wordlist
-        # if filter(word)
-            # newlist.append word
-    #return new list
-    return
+    newwordlist = {}
 
+    #for word in wordlist
+    for word in wordlist:
+        # if filter(word)
+        if filter_function(word):
+            # newlist.append word
+            newwordlist[word] = wordlist[word]
+    
+    #return new list
+    return newwordlist
 
 #return true if word is one of the 100 most common words in the english language
 def is_common_word(word):
     #if word in commons
-    return
+    return word in commons
 
+def word_is(compare_word):
+    def fn(wrd):
+        return wrd == compare_word
+    return fn
