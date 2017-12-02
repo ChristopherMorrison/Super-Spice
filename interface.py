07d8b9bcb0caa08df6ea2a85b83eab5cd6126256
Chris Jantzen <jantzeca@mail.uc.edu>
2017-12-02 18:11:55 -0500

    fixed your errors and removed unused libraries.

--- interface.py
+++ interface.py
@@ -2,14 +2,10 @@ import tkinter as Tk
 from PIL import Image, ImageTk
 from functools import partial
 from matplotlib import *
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-import numpy as np
-from web_scrape import *
-import matplotlib.pyplot as plt
 import pylab
 from data_filter import *
-
+from web_scrape import *
 
 
 def buttonAction(num):
@@ -33,30 +29,28 @@ def buttonAction(num):
 
 def main():
     # initialize a window
-    root = Tk.Tk()  
+    root = Tk.Tk()
 
     # create a frame
     global frame
-    frame = Tk.Frame()  
+    frame = Tk.Frame()
 
     # title and background color
-    root.title('Wired.com Trends')  
+    root.title('Wired.com Trends')
     root.configure(background='gray')
 
     # insert picture at the top
-    img = Image.open("./wired.jpg")  
+    img = Image.open("./wired.jpg")
     img = img.resize((250, 50), Image.ANTIALIAS)
     photo = ImageTk.PhotoImage(img)
     imgLabel = Tk.Label(image=photo)
-
-    # extends all 7 columns
-    imgLabel.grid(row=0, columnspan=7)  
+    imgLabel.grid(row=0, columnspan=7)  # extends all 7 columns
 
     # creates buttons for each section of wired.com
     # when pressed, sends a value to the buttonAction function
-    Labels = ["Business", "Culture", "Design", "Gear", "Science", "Security", "Security", "Transportation"]
+    Labels = ["Business", "Culture", "Design", "Gear", "Science", "Security", "Transportation"]
     for i in range(len(Labels)):
-        btn = Tk.Button(root, text=Labels[i], height=3, width=10, command=partial(buttonAction, i+1))
+        btn = Tk.Button(root, text=Labels[i], height=3, width=10, command=partial(buttonAction, i + 1))
         btn.grid(row=1, column=i, padx=5, pady=5)
 
     # creates a changable message label in the row below the buttons.
@@ -65,14 +59,14 @@ def main():
     Tk.Label(root, textvariable=message, bg='gray').grid(row=2, column=0, padx=5, pady=5, columnspan=7)
 
     # bar graph
-    number_of_words_to_show_because_we_dont_hardcode_numbers_without_documentation_jantzen = 7
-    x = range(number_of_words_to_show_because_we_dont_hardcode_numbers_without_documentation_jantzen)
-    y = range(number_of_words_to_show_because_we_dont_hardcode_numbers_without_documentation_jantzen)
+    words = 7
+    x = range(words)
+    y = range(words)
     f = pylab.figure()
     ax = f.add_axes([.1, .1, .8, .8])
     ax.bar(x, y, align='center')
     ax.set_xticks(x)
-    ax.set_xticklabels(['word']*number_of_words_to_show_because_we_dont_hardcode_numbers_without_documentation_jantzen)
+    ax.set_xticklabels(['word'] * words)
     canvas = FigureCanvasTkAgg(f, root)
     canvas.show()
     canvas.get_tk_widget().grid(row=3, column=0, padx=5, pady=15, columnspan=7)
@@ -83,6 +77,7 @@ def main():
     toolbar.update()
 
     root.mainloop()
+
 
 message = None
 frame = None
